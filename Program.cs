feec73b324e4c020179f6eda9ef5520a2a12e63f
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2021-04-19 16:54:30 +0300

    Пытаемся читать жсон

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace lotr_redactor_console
 {
@@ -9,14 +12,39 @@ namespace lotr_redactor_console
         {
             string path;
 
+            Dictionary<int, string> filesNames;
+            Dictionary<int, string> fileHeroes;
+
+
             Console.WriteLine("путь к файлам игры:");
             path = Console.ReadLine();
 
             string[] files = Directory.GetFiles(path, "SavedGameA", SearchOption.AllDirectories);
 
+            filesNames = new Dictionary<int, string>(files.Length);
+
+            Console.WriteLine("Список сохранений:");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                using (StreamReader reader = File.OpenText(files[i]))
+                {
+                    JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+                    string PartyName = (string)o.SelectToken("PartyName");
+                    Console.WriteLine($"{i}. {PartyName}");
+
+                    var heroes = o.SelectToken("HeroInfo");
+                    foreach(var hero in heroes)
+                    {
+                        var id = (int)hero.SelectToken("Id");
+                    }
+
+                    filesNames.Add(i, files[i]);
+                }
+            }
+
             Console.ReadLine();
-
-
         }
     }
 }
