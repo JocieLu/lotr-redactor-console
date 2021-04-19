438657fb16623d1204fb068e0d6a1a21e3fa681d
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2021-04-19 16:01:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lotr_redactor_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+
+            Console.WriteLine("путь к файлам игры:");
+            path = Console.ReadLine();
+
+
+        }
+    }
+}
