84be0ac76bc6ae4a5fc6b927c5b8fa1fdfb0aa5c
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2021-04-19 16:59:11 +0300

    Добавил классы для сериализации героев

---
+++ Classes/AvailableXP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lotr_redactor_console.Classes
+{
+    class AvailableXP
+    {
+        public int RoleId { get; set; }
+        public int XP { get; set; }
+    }
+}
---
+++ Classes/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lotr_redactor_console.Classes
+{
+    class Hero
+    {
+        public int Id { get; set; }
+        public int TitleId { get; set; }
+        public int Status { get; set; }
+        public int LastStandDifficulty { get; set; }
+        public int RoleId { get; set; }
+        public int Corruption { get; set; }
+
+        List<int> HeroItemIds { get; set; }
+        List<int> UnlockedTitleIds { get; set; }
+        List<int> EquippedSkillIds { get; set; }
+
+        List<AvailableXP> AvailableXP { get; set; }
+    }
+}
